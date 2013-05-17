0c4a4941b62e3076c1e220b698b644cc67cd45b4
Jed Hunsaker <jed.hunsaker@gmail.com>
2013-05-16 19:41:38 -0700

    Paint CodePainter code for the first time

--- codepainter.js
+++ codepainter.js
@@ -1,76 +1,76 @@
-var fs = require( 'fs' );
+var fs = require('fs');
 
-var Pipe = require( './lib/Pipe' );
-var rules = require( './lib/rules' );
-var Serializer = require( './lib/Serializer' );
-var Tokenizer = require( './lib/Tokenizer' );
+var Pipe = require('./lib/Pipe');
+var rules = require('./lib/rules');
+var Serializer = require('./lib/Serializer');
+var Tokenizer = require('./lib/Tokenizer');
 
 
 module.exports = {
-	infer : function( sample, callback ) {
+	infer : function(sample, callback) {
 		var style = {};
 		var tokenizer = new Tokenizer();
 
-		sample.pipe( tokenizer );
+		sample.pipe(tokenizer);
 
-		rules.forEach( function( rule ) {
-			rule.infer( tokenizer, function( inferredStyle ) {
-				Object.keys( inferredStyle ).forEach( function( key ) {
-					style[ key ] = inferredStyle[ key ];
-				} );
-			} );
-		} );
+		rules.forEach(function(rule) {
+			rule.infer(tokenizer, function(inferredStyle) {
+				Object.keys(inferredStyle).forEach(function(key) {
+					style[key] = inferredStyle[key];
+				});
+			});
+		});
 
-		tokenizer.on( 'end', function() {
-			tokenizer.registerRules( style );
-			callback( style );
-		} );
+		tokenizer.on('end', function() {
+			tokenizer.registerRules(style);
+			callback(style);
+		});
 
 		sample.resume();
 	},
 
-	transform : function( input, style, output, callback ) {
+	transform : function(input, style, output, callback) {
 		var enabledRules = [];
 		var tokenizer = new Tokenizer();
 		var serializer = new Serializer();
 		var streams = [];
 
-		style = this.convertStyle( style );
-		if( style.hasOwnProperty( 'indent_style' ) ) {
+		style = this.convertStyle(style);
+		if(style.hasOwnProperty('indent_style')) {
 			style.indent_style_and_size = true;
 		}
-		rules.forEach( function( iRule ) {
-			if( iRule.name in style ) {
-				enabledRules.push( iRule );
+		rules.forEach(function(iRule) {
+			if(iRule.name in style) {
+				enabledRules.push(iRule);
 			}
-		} );
+		});
 
-		input.pipe( tokenizer );
-		serializer.pipe( output );
-		serializer.on( 'end', function() {
-			if( typeof callback === 'function' ) {
+		input.pipe(tokenizer);
+		serializer.pipe(output);
+		serializer.on('end', function() {
+			if(typeof callback === 'function') {
 				callback();
 			}
-		} );
+		});
 
-		if( enabledRules.length > 0 ) {
+		if(enabledRules.length > 0) {
 
-			tokenizer.registerRules( enabledRules );
+			tokenizer.registerRules(enabledRules);
 
-			streams.push( tokenizer );
+			streams.push(tokenizer);
 
-			for( var i = 0; i < enabledRules.length - 1; i++ )
-				streams.push( new Pipe() );
+			for(var i = 0; i < enabledRules.length - 1; i++)
+				streams.push(new Pipe());
 
-			streams.push( serializer );
+			streams.push(serializer);
 
 			var errorFunction = function(){};
-			for( i = 0; i < enabledRules.length; i++ ) {
-				var rule = enabledRules[ i ];
-				rule.transform( streams[ i ], style, streams[ i + 1 ], errorFunction );
+			for(i = 0; i < enabledRules.length; i++) {
+				var rule = enabledRules[i];
+				rule.transform(streams[i], style, streams[i + 1], errorFunction);
 			}
 		} else {
-			tokenizer.pipe( serializer );
+			tokenizer.pipe(serializer);
 		}
 		input.resume();
 	},
@@ -83,24 +83,24 @@ module.exports = {
 	 * the respective style file. If that does not work either, throws an error.
 	 *
 	 */
-	convertStyle : function( style ) {
+	convertStyle : function(style) {
 		try {
-			return typeof style === 'string' ? JSON.parse( style ) : style;
-		} catch( e ) {
-			try {
-				return require( __dirname + '/lib/styles/' + style + '.json' );
-			} catch( e2 ) {
+			return typeof style === 'string' ? JSON.parse(style) : style;
+		} catch(e) {
+				try {
+					return require(__dirname + '/lib/styles/' + style + '.json');
+				} catch(e2) {
 
-				var msg = style + ' is not a valid style.\n\nValid predefined styles are:\n';
+						var msg = style + ' is not a valid style.\n\nValid predefined styles are:\n';
 
-				var files = fs.readdirSync( __dirname + '/lib/styles/' );
+						var files = fs.readdirSync(__dirname + '/lib/styles/');
 
-				for( var i in files ) {
-					msg += '  ' + files[ i ].slice( 0, -5 ) + '\n';
-				}
+						for(var i in files) {
+							msg += '  ' + files[i].slice(0, - 5) + '\n';
+						}
 
-				throw new Error( msg );
+						throw new Error(msg);
+					}
 			}
-		}
 	}
 };
