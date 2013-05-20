c402e4c863f5dac592c139ad6afd2abf77f43965
Jed Hunsaker <jed.hunsaker@gmail.com>
2013-05-20 16:35:17 -0700

    Add infer, cascade, async to Transformer

--- codepainter.js
+++ codepainter.js
@@ -3,14 +3,11 @@ var Transformer = require('./lib/Transformer');
 
 
 module.exports = {
-
 	infer : function(samplePath, callback, Rule) {
-		var inferrer = new Inferrer();
-		inferrer.infer( samplePath, callback, Rule );
+		new Inferrer().infer( samplePath, callback, Rule );
 	},
 
-	transform : function(inputPath, style, callback, isTesting) {
-		var transformer = new Transformer();
-		transformer.transform( inputPath, style, callback, isTesting );
+	transform : function(options, callback) {
+		new Transformer().transform( options, callback );
 	}
 };
